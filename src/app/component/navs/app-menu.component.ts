5d3b4c097683823c48f7c5d283742ca6c438d14e
Wael Katrou <waelkatrouensi@gmail.com>
2019-03-01 14:36:32 +0100

    Add menu nav bar to the main page

---
+++ src/app/component/navs/app-menu.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './app-menu.component.html',
+  styleUrls: ['./app-menu.component.css']
+})
+export class AppMenuComponent implements OnInit {
+
+  currentTab : number = TAB_LIST.HOME;
+  tabList = TAB_LIST;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+const TAB_LIST = {
+  HOME : 1,
+  PROFILE : 2,
+  NEW_REQUEST : 3
+};
